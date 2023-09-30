6b53a1770df53c50fb96704d2c06d94357f9f914
Arkadiy Abramovich <abramovicharkadiy@gmail.com>
2023-09-30 09:20:51 +0700

    add changes

--- building_connecting_libs/task3/Leaver.h
+++ building_connecting_libs/task3/Leaver.h
@@ -1,4 +1,9 @@
 #pragma once
+// #ifndef LEAVE_LIB_EXPORTS
+//     #define LEAVE_LIB_API __declspec(dllexport)
+// #else
+//     #define LEAVE_LIB_API __declspec(dllimport)
+// #endif
 
 #include <string>
 
